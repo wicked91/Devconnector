5bddb13ae072c7c612f0b36ab0817ab8e388ba36
wicked91 <wicked91j@gmail.com>
2019-04-05 14:52:04 +0900

    'fixgitignore'

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://admin:1q2w3e4r@devconnector-oglha.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://admin:1q2w3e4r@devconnector-oglha.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
